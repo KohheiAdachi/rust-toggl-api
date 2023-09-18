b0fbbf55f8ed2713fbcc0a392d34ae990bd5014a
KohheiAdachi <kohei.adachi805@mail.kyutech.jp>
2023-09-18 20:23:14 +0900

    Add json parse

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,28 @@ extern crate tokio;
 use reqwest::header::CONTENT_TYPE;
 use reqwest::Client;
 use reqwest::Method;
+use serde::Deserialize;
+#[derive(Debug, Deserialize)]
+struct TimeEntry {
+    at: String,
+    billable: bool,
+    description: String,
+    duration: i64,
+    duronly: bool,
+    id: i64,
+    pid: i64,
+    project_id: Option<i64>,
+    server_deleted_at: Option<String>,
+    start: String,
+    stop: Option<String>,
+    tag_ids: Vec<i64>,
+    tags: Vec<String>,
+    task_id: Option<i64>,
+    uid: i64,
+    user_id: i64,
+    wid: i64,
+    workspace_id: i64,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
@@ -12,7 +34,7 @@ async fn main() -> Result<(), reqwest::Error> {
         Err(_e) => "dev".to_string(),
     };
 
-    let json = client
+    let res = client
         .request(
             Method::GET,
             "https://api.track.toggl.com/api/v9/me/time_entries".to_string(),
@@ -23,7 +45,9 @@ async fn main() -> Result<(), reqwest::Error> {
         .send()
         .await?;
 
-    println!("{:#?}", json);
+    // let body = res.text().await?;
+    let body = res.json::<Vec<TimeEntry>>().await?;
+    println!("{:?}", body);
 
     Ok(())
 }
